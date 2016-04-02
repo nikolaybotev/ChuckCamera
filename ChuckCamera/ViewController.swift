a6a37d419eafed8a2568a660969b92acd53d8d5e
Nikolai Botev <nikolai_botev@yahoo.com>
2016-04-02 14:59:15 -0700

    Final version with camera detection

--- ChuckCamera/ViewController.swift
+++ ChuckCamera/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if (!UIImagePickerController.isSourceTypeAvailable(.Camera)) {
+            let alertView = UIAlertView(
+                title: "Error", message: "No camera", delegate: nil, cancelButtonTitle: "OK")
+            alertView.show()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func imagePickerController(picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let chosenImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        self.imageView.image = chosenImage
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
     }
 
     @IBAction func takePhoto(sender: UIButton) {
